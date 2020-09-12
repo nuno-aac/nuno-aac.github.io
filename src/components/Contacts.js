10c25fece75aabcac9be4fd4f7b2e5cad64fee11
CoelhoLado <naac.1999@gmail.com>
2020-09-12 14:20:49 +0100

    favicon/ newtabs

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -12,12 +12,12 @@ function Contacts(props) {
     return (
         <div className='ContactsBox'>
             <div className='Socials'>
-                <a href='https://www.facebook.com/nuno.cunha.144'>
+                <a target="_blank" rel="noopener noreferrer" href='https://www.facebook.com/nuno.cunha.144'>
                     <div className='Contact' >
                         <FontAwesomeIcon className='ContactIcon' icon={faFacebookSquare} />
                     </div>
                 </a>
-                <a href='https://github.com/nuno-aac'>
+                <a target="_blank" rel="noopener noreferrer" href='https://github.com/nuno-aac'>
                     <div className='Contact' >
                         <FontAwesomeIcon className='ContactIcon' icon={faGithubSquare} />
                     </div>
